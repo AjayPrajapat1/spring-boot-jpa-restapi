56fe403b0ac1d0baa8133cffef18bf2995a7c2a4
Ajay Prajapat <ajay.prajapat.4566@gmail.com>
2020-09-24 12:59:17 +0530

    final commit. add all operations..

--- SpringBootJpa/src/main/java/com/aj/springbootjpa/controller/EmployeeController.java
+++ SpringBootJpa/src/main/java/com/aj/springbootjpa/controller/EmployeeController.java
@@ -1,12 +1,19 @@
 package com.aj.springbootjpa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aj.springbootjpa.dao.EmployeeDao;
+import com.aj.springbootjpa.model.Employee;
 
 @RestController
 public class EmployeeController {
@@ -15,7 +22,39 @@ public class EmployeeController {
 	private EmployeeDao employeeDao;
 
 	@GetMapping("/")
-	public List getList() {
+	public List<Employee> getList() {
 		return employeeDao.findAll();
+	}
+	
+	@PostMapping("/employee")
+	public Employee save(@RequestBody Employee employeeObj) {
+		return employeeDao.save(employeeObj);
+	}
+	
+	@GetMapping("/employee/{id}")
+	public Employee get(@PathVariable long id) {
+		Optional<Employee> employee = employeeDao.findById(id);
+		if(employee.isPresent()) {
+			return employee.get();
+		}else {
+			throw new RuntimeException("Employee not found for the id "+id);
+		}
+	}
+	
+	@DeleteMapping("/employee/{id}")
+	public String delete(@PathVariable long id) {
+		Optional<Employee> employee = employeeDao.findById(id);
+		if(employee.isPresent()) {
+			employeeDao.delete(employee.get());
+			return "Employee is deleted with id "+id;
+		}else {
+			throw new RuntimeException("Employee not found for the id "+id);
+		}
+	}
+	
+	@PutMapping("/employee/{id}")
+	public Employee update(@RequestBody Employee employeeObj, @PathVariable long id) {
+		Employee isExist = employeeDao.findById(id).orElseThrow(()-> new RuntimeException("employee id not found"));
+		return employeeDao.save(employeeObj);
 	}
 }
